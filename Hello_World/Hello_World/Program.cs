04cceaa682f4e3c7fdfe7baedeb2706f2ee8221a
BuildTools <unconfigured@null.spigotmc.org>
2016-10-10 15:42:25 -0300

    Simple Hello_World application in C#, with annotations.

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call Console class's WriteLine() method, passing string arg
+            Console.WriteLine("Hello world!");
+            //Waits for any input from keyboard before closing console window
+            Console.ReadKey();
+        }
+    }
+}
